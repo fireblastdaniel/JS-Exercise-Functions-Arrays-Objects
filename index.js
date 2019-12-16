7b00b084836b224b3b290e023cbad7e144507395
Daniel <fireblastdaniel@yahoo.com>
2019-12-16 13:05:11 -0800

    passed makeSmartPerson tests

--- index.js
+++ index.js
@@ -61,8 +61,8 @@ function makePersonObject(input_id, input_name, input_email) {
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(/* code here */) {
-  /* code here */
+function getName(person) {
+  return 'Hello, my name is ' + person.name;
 }
 
 /**
@@ -78,8 +78,13 @@ function getName(/* code here */) {
  *         and returns a string like `Hello, my name is {name}`.
  *         where `{name}` is the name passed into `makeSmartPerson`.
 */
-function makeSmartPerson(/* code here */) {
-  /* code here */
+function makeSmartPerson(_name) {
+  const person = {
+    name: _name,
+    sum : function(num1, num2) {return num1 + num2;},
+    speak: function() {return 'Hello, my name is ' + this.name}
+  }
+  return person;
 }
 
 
